00fb7a65a0b618666df751f5ebbda211df98e084
Grzegorz Czyrnek <grzegorz.czyrnek@gmail.com>
2024-11-09 17:40:01 +0100

    Changes: - add tasks.json - add points in plot - set line width and point size - add automatic range for plotting

--- plotter/src/lib.rs
+++ plotter/src/lib.rs
@@ -1,23 +1,28 @@
+use core::f32;
+
 use plotters::prelude::*;
 
 const RESOLUTION : (u32, u32) = (2048, 1280);
+const WIDTH : u32 = 4;
+const P_SIZE: u32 = 6;
 
-pub fn plot_data(x: &[f32], y: &[f32], title: &str, max: f32) -> Result<(), Box<dyn std::error::Error>> {
-    let filename = &format!("{}.png", title.split(" ").collect::<Vec<&str>>()[0]);
+pub fn plot_data(x: &[f32], y: &[f32], title: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let filename = &format!("{}.png", title.split(" ").collect::<Vec<&str>>().join("_"));
     let backend = BitMapBackend::new(filename, RESOLUTION).into_drawing_area();
     backend.fill(&WHITE)?;
+    let (min, max) = get_min_max(y);
     let mut chart = ChartBuilder::on(&backend)
         .caption(title, ("sans-serif", 50).into_font())
         .margin(15)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(0f32..*x.last().unwrap(), 0f32..max)?;
+        .build_cartesian_2d(x[0]..*x.last().unwrap(), min..max)?;
     chart.configure_mesh().draw()?;
     let data = x.iter().zip(y.iter()).map(|(&x, &y)| (x, y));
     chart.draw_series(LineSeries::new(
             data,
-            &MAGENTA,
-        ))?
+            MAGENTA.stroke_width(WIDTH).filled(),
+        ).point_size(P_SIZE))?
         .label(title)
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
 
@@ -30,8 +35,27 @@ pub fn plot_data(x: &[f32], y: &[f32], title: &str, max: f32) -> Result<(), Box<
     Ok(())
 }
 
+fn get_min_max(data: &[f32]) -> (f32, f32) {
+    let mut min = f32::MAX;
+    let mut max = f32::MIN;
+    for x in data {
+        if *x > max {
+            max = *x;
+        }
+        if *x < min {
+            min = *x;
+        }
+    }
+    (min, max)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    #[test]
+    fn get_min_max_shall_return_minimum_and_maximum_value_of_slice() {
+        assert_eq!(get_min_max(&[0.0, 1.1, 2.2, 3.3]), (0.0, 3.3));
+        assert_eq!(get_min_max(&[343525.5, -431421.32, 43.45, 44234.2352, 235232234.3, 0.1241241]),
+            (-431421.32, 235232234.3));
+    }
 }
